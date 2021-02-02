0aa21d9ea2fbaf77da3734d3dcbf1fe54f3c9fa0
aduar2 <77691520+aduar2@users.noreply.github.com>
2021-02-01 20:35:48 -0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$("#h1").click(function() {
 		$("p").css({"color":"red", "background-color": "yellow"});
-		$("img").css("border","25px solid black");
+		$("img").css({"border","25px solid black"});
 	});
 	
 	$("#p1").click(function() {
